7742e7ee0d04d6d015c4c65b1d565722e37ebb37
Lcs-Knsk <84292789+Lcs-Knsk@users.noreply.github.com>
2023-10-29 15:56:56 -0400

    Configuration

--- lib/config.dart
+++ lib/config.dart
@@ -8,6 +8,12 @@ class Config extends StatefulWidget{
 
 class _ConfigState extends State<Config> {
   TextEditingController _confidence = TextEditingController(text: Globals.confidence.toString());
+  TextEditingController _max = TextEditingController(text: '5');
+
+  dispose(){
+    _confidence.dispose();
+    _max.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +26,9 @@ class _ConfigState extends State<Config> {
         child: Column(
 
           children:[
-
             Row(
               children: [
-                Expanded(
+                const Expanded(
                   child: Text(
                       "Enter confidence rate for the AI (Values from 0-1): ",
                       textAlign: TextAlign.left,
@@ -44,9 +49,32 @@ class _ConfigState extends State<Config> {
                   ),
                 ),
               ],
+            ),
+
+            Row(
+              children: [
+                const Expanded(
+                  child: Text(
+                    "Enter confidence max detections in one image: ",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.visible,
+                  ),
+                ),
+
+                Container(
+
+                  width: 30,
+                  child: TextField(
+                    controller: _max,
+                    maxLength: 6,
+                    textDirection: TextDirection.ltr,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
             )
-
-
 
         ]
     ),
